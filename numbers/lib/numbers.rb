aeea1362ef9de86c1ebc2416a5147525d8d7dfa9
Pavani <pavani@railsfactory.org>
2014-07-07 12:02:43 +0530

    wrote the test for 1 and 21 and made it to pass

---
+++ numbers/lib/numbers.rb
@@ -0,0 +1,8 @@
+class Numbers
+
+  def self.convert(number)
+    if number == 1||21||31
+     return number.to_s + "st"
+    end
+  end
+end 
---
+++ numbers/test/numbers_test.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/numbers'
+require 'test/unit'
+
+
+class TestNumbers < Test::Unit::TestCase
+
+
+
+  def test_num
+    assert_equal '1st',Numbers.convert(1)
+    assert_equal '21st',Numbers.convert(21)
+  end
+
+
+
+end 
