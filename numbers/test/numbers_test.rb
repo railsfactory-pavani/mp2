414e0a0f830afaa21ca9cecdfdb2ef2393be8d44
Pavani <pavani@railsfactory.org>
2014-07-07 12:34:44 +0530

    wrote the test for 1 and 2 and made to pass

--- numbers/test/numbers_test.rb
+++ numbers/test/numbers_test.rb
@@ -8,7 +8,8 @@ class TestNumbers < Test::Unit::TestCase
 
   def test_num
     assert_equal '1st',Numbers.convert(1)
-    assert_equal '21st',Numbers.convert(21)
+    assert_equal '2nd',Numbers.convert(2)
+    
   end
 
 
