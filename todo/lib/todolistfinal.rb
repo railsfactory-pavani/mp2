ef92334de6e177d86e1ae438e2f7a1e18e6402c4
Pavani <pavani@railsfactory.org>
2014-07-11 16:52:01 +0530

    passed tests for all test scenarios

--- todo/lib/todolistfinal.rb
+++ todo/lib/todolistfinal.rb
@@ -1,72 +1,79 @@
 class Todolist
-
-   $pending = []
-   $todo = []
-   $completed = []
- attr_accessor :filename
    
 
  def initialize(filename)
    filename = filename
-   
+   @pending = []
+   @todo = []
+   @completed = []
    
  end
 
  def pending
-  return $pending.size
+   @pending
  end
 
  def list
-  $todo = $pending + $completed
-  return $todo.size
+  @todo = @pending + @completed
+    @todo
  end
+
+
 
  def completed
-   return $completed.size
+    @completed
  end
+
+
 
  def add(title)
-  $pending << title
-  return $pending.count
+  @pending << title
+  @pending
  end
+
+
 
  def complete(num)
- $completed << $pending[num - 1]
- $pending.delete_at(num - 1)
- return $completed.size
+ @completed << @pending[num - 1]
+ @pending.delete_at(num - 1)
+ @completed
  end
 
-#def self.incomplete(num)
-#var = $todo[num]
-#$pending << var
-#$completed.delete var
-#end
+
 
  def delete(num)
-  $completed.delete_at(num - 1)
-  return $completed.size
+  @completed.delete_at(num - 1)
+   @completed
  end
+
+
 
  def modify1(num,title)
-  $pending[num - 1] = title
-  return $pending[num - 1]
+  @pending[num - 1] = title
+  return @pending[num - 1]
  end
 
+ 
+
  def empty
-  $pending.clear
-  $completed.clear
-  $todo.clear
+  @pending = []
+  @completed = []
+  @todo = []
   return true
  end
 
+ 
+
  def show_pending(num)
-  return $pending[num - 1]
+  return @pending[num - 1]
  end
+
+
 
  def show_completed(num)
-  return $completed[num - 1]
+  return @completed[num - 1]
  end
 
+
 end
-$t = Todolist.new("todop.txt")
 
--- todo/test/todolistfinal_test.rb
+++ todo/test/todolistfinal_test.rb
@@ -5,80 +5,143 @@ require 'test/unit'
 
 class TestTodo < Test::Unit::TestCase
 
- $t = Todolist.new("todop.txt")
+ def setup
+
+  @t = Todolist.new("todop.txt")
  
- def test_aaempty
- 
-  assert_equal true,$t.empty
-  assert_equal 0,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 0,$t.list
  end
+
+ def teardown
+
+  @t=nil
+ 
+ end
+
+ 
+ def test_aempty
+ 
+  @t.empty
+  assert_equal 0,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 0,@t.list.size
+ end
+
+
  def test_add1
-  assert_equal 1,$t.add("open")
-  assert_equal 1,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 1,$t.list
+  @t.empty
+  @t.add("one")
+  assert_equal 1,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 1,@t.list.size
  end
+
+
 
  def test_add2
-  assert_equal 2,$t.add("search")
-  assert_equal 2,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 2,$t.list
- end
- 
-  def test_add3
-  assert_equal 3,$t.add("read")
-  assert_equal 3,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 3,$t.list
+  @t.empty
+  @t.add("one")
+  @t.add("two")
+  assert_equal 2,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 2,@t.list.size
  end
 
-  def test_add4
-  assert_equal 4,$t.add("write")
-  assert_equal 4,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 4,$t.list
- end
- 
-  def test_add5
-  assert_equal 5,$t.add("store")
-  assert_equal 5,$t.pending
-  assert_equal 0,$t.completed
-  assert_equal 5,$t.list
+
+
+ def test_complete
+  @t.empty
+  @t.add("one")
+
+  assert_equal 1,@t.pending.size
+  assert_equal 0,@t.completed.size
+  assert_equal 1,@t.list.size
+
+  @t.complete(1)
+
+  assert_equal 0,@t.pending.size
+  assert_equal 1,@t.completed.size
+  assert_equal 1,@t.list.size
  end
 
-  def test_complete
-  assert_equal 1,$t.complete(1)
-  assert_equal 2,$t.complete(2)
-  assert_equal 3,$t.pending
-  assert_equal 2,$t.completed
-  assert_equal 5,$t.list
- end
+
 
   def test_delete
-  assert_equal 1,$t.delete(2)
-  assert_equal 3,$t.pending
-  assert_equal 1,$t.completed
-  assert_equal 4,$t.list
- end
- 
-  def test_modify
-  assert_equal "merge",$t.modify1(2,"merge")
-  assert_equal "load",$t.modify1(3,"load")
-  assert_equal 3,$t.pending
-  assert_equal 1,$t.completed
-  assert_equal 4,$t.list
- end
-  
-  def test_pending
-   assert_equal "merge",$t.show_pending(2)
-   assert_equal "load",$t.show_pending(3)
-  end
+   @t.empty
+   @t.add("one")
+   @t.complete(1)
    
+   assert_equal 0,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 1,@t.list.size
+
+   @t.delete(1)
+
+   assert_equal 0,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 0,@t.list.size
+
+  end
+ 
+  
+
+ def test_modify
+   @t.empty
+   @t.add("one")
+   @t.add("two")
+   @t.complete(1)
+
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.modify1(1,"three")
+  
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+  end
+ 
+
+ 
+  def test_pending
+   @t.empty
+   @t.add("one")
+   @t.add("two")
+  
+
+   assert_equal 2,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.show_pending(2)
+     
+   assert_equal "two",@t.show_pending(2)
+   assert_equal 2,@t.pending.size
+   assert_equal 0,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+  end
+  
+
+ 
   def test_qcompleted
-   assert_equal "open",$t.show_completed(1)
+   @t.empty
+   @t.add("one")
+   @t.add("two")
+   @t.complete(1)
+
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+   
+   @t.show_completed(1)
+     
+   assert_equal "one",@t.show_completed(1)
+   assert_equal 1,@t.pending.size
+   assert_equal 1,@t.completed.size
+   assert_equal 2,@t.list.size
+ 
   end
   
 end
